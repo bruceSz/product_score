4a964a5176b8cc178d6996d6aff0053f89436ced
bruceSz <zhangsong5@jd.com>
2017-02-26 15:18:36 +0800

    add plot test

--- statistic/generate_nd.py
+++ statistic/generate_nd.py
@@ -8,6 +8,23 @@ def generate_gauss_number(mu, sigma):
     while True:
         yield random.gauss(mu, sigma)
 
+def sorted_gauss_numbers(mu, sigma, size):
+    gn = generate_gauss_number(mu, sigma)
+    r_list = []
+    for i in range(0,size):
+        r_list.append(gn.next())
+
+    r_list.sort()
+    return r_list
+
+def raw_gauss_numbers(mu, sigma, size, round_length):
+    gn = generate_gauss_number(mu, sigma)
+    l = []
+    for i in range(0,size):
+        l.append(round(gn.next(),round_length))
+    return l
+
+
 if __name__ == "__main__":
     gn = generate_gauss_number(1,1)
     r_list = []
--- statistic/simple_plot.py
+++ statistic/simple_plot.py
@@ -1,5 +1,8 @@
+
+
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-plt.show()
+
 plt.bar(left = 0, height = 1)
+plt.show()
