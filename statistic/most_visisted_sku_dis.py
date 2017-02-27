38e429da5d5dcebe85b386423e7277102b53dc01
bruceSz <zsyuyizhang@gmail.com>
2017-02-27 14:49:26 +0800

    add curve fit

--- statistic/most_visisted_sku_dis.py
+++ statistic/most_visisted_sku_dis.py
@@ -1,6 +1,7 @@
 
 from pylab import *
 import numpy
+from scipy.optimize import curve_fit
 
 
 import utils
@@ -18,12 +19,50 @@ def dis_sorted_log_of_visited_sku():
     plot( l_log, X)
     show()
 
+
+def dis_sorted_of_visited_sku_fit_curve():
+    l = []
+    with open('../test_datas/most_visited_sku', 'r') as f:
+        for line in f:
+            l.append(int(line.strip()))
+    l.sort()
+    l_f = [i - l[0] for i in l]
+
+    def func(xdata, a, b):
+        return a**x+b
+
+    x = np.linspace(0,len(l_f),len(l_f))
+    print len(x)
+    popt, pcov = curve_fit(func, x, l_f)
+    y2 = [func(i, popt[0],popt[1]) for i in x]
+    plot(x,l_f,'b-')
+    plot(x,y2,'r-')
+    print x[:10]
+    print l_f[:10]
+    print y2[0][:10]
+    show()
+
+
+def dis_sorted_of_visited_sku_fit_poly():
+    l = []
+    with open('../test_datas/most_visited_sku_1000', 'r') as f:
+        for line in f:
+            l.append(int(line.strip()))
+    l.sort()
+    l_f = [i - l[0] for i in l]
+    x = np.linspace(0,1,len(l_f))
+    fit_factor = np.polyfit(x,l_f,5)
+    p = np.poly1d(fit_factor)
+    plot(x,l_f,'o',x,p(x),lw=1)
+    show()
+
 def dis_sorted_of_visited_sku_scatter():
     l = []
     with open('../test_datas/most_visited_sku_1000', 'r') as f:
         for line in f:
             l.append(int(line.strip()))
     l.sort()
+
     for i in l:
         print "|", i, "|"
     n = len(l)
@@ -68,7 +107,7 @@ def dis_of_visited_sku():
     l_new = [(i - mean) for i in l]
 
     d = utils.compute_frequency(l_new)
-    plt.sca
+
     plt.scatter(d.keys(), d.values(), s=50,alpha=0.5, cmap=plt.cm.hsv)
 
     for k, v in d.iteritems():
@@ -79,7 +118,9 @@ def dis_of_visited_sku():
 
     show()
 if __name__ == "__main__":
-    dis_sorted_of_visited_sku_scatter()
+    #dis_sorted_of_visited_sku_fit_poly()
+    dis_sorted_of_visited_sku_fit_curve()
+    #dis_sorted_of_visited_sku_scatter()
     #dis_sorted_log_of_visited_sku()
-   #dis_sorted_of_visited_sku()
+    #dis_sorted_of_visited_sku()
     #dis_of_visited_sku()
--- statistic/test_fitting.py
+++ statistic/test_fitting.py
@@ -1,15 +1,42 @@
 import numpy
 import matplotlib.pyplot as plt
-import pylab
+from pylab import *
+from scipy.optimize import curve_fit
+
+
 def test_linear_fit():
     x = [i for i in range(1, 10)]
     y = x + numpy.random.randn(len(x))
     plt.scatter(x,y)
     pa = numpy.polyfit(x,y,1)
     y1 = numpy.poly1d(pa)
-    axis = pylab.np.linspace(1, 10, 10, endpoint=True)
+    axis = np.linspace(1, 10, 10, endpoint=True)
     plt.plot(axis,y1(axis))
     plt.show()
 
+
+def test_cos_fit():
+    x =  np.linspace(0,1,50)
+    y =  numpy.cos(x)+0.1* numpy.random.rand(50)
+    cof = numpy.polyfit(x,y,3)
+    p = numpy.poly1d(cof)
+    plt.plot(x,y,'o',x,p(x),lw=2)
+    plt.show()
+
+def exponent_fit():
+
+    def func(x, a, b ,c):
+        return a*numpy.exp(-b*x)+c
+
+    xdata = numpy.linspace(0,4,50)
+    y = func(xdata, 2.5, 1.3, 0.5)
+    ydata = y+0.1*numpy.random.randn(len(xdata))
+    popt , pcov = curve_fit(func, xdata, ydata)
+    y2 = [func(i,popt[0],popt[1],popt[2]) for i in xdata]
+    plt.plot(xdata,ydata,'o',xdata,y2,lw=1)
+    plt.show()
+
 if __name__ == "__main__":
-    test_linear_fit()
+    #test_linear_fit()
+    #test_cos_fit()
+    exponent_fit()
